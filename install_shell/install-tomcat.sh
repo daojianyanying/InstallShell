b2dc5b59649772ef441833e2de4a99771bd15dcb
liuxiang <liu3574226@163.com>
2021-03-21 21:02:54 +0800

    格式转换

--- install_shell/install-tomcat.sh
+++ install_shell/install-tomcat.sh
@@ -11,11 +11,11 @@
 #开始业务逻辑
 #下载软件
 mkdir -p /opt/software/tomcat/
-wget -P /opt/software/tomcat/ https://mirror.bit.edu.cn/apache/tomcat/tomcat-8/v8.5.63/bin/apache-tomcat-8.5.63.tar.gz
+wget -P /opt/software/tomcat/ https://mirrors.bfsu.edu.cn/apache/tomcat/tomcat-8/v8.5.64/bin/apache-tomcat-8.5.64.tar.gz
 
 #解压软件==安装
 mkdir -p /usr/local/tomcat/
-tar -zxvf /opt/software/tomcat/apache-tomcat-8.5.63.tar.gz -C /usr/local/tomcat/
+tar -zxvf /opt/software/tomcat/apache-tomcat-8.5.64.tar.gz -C /usr/local/tomcat/
 
 #配置环境(tomcat不涉及)
 #echo "export PATH=$PATH:/usr/local/tomcat/apache-tomcat-8.5.63/bin" >> /etc/profile
