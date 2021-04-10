331b97c85383f9f40d24a1868053fbdd3e7b374e
liuxiang <liu3574226@163.com>
2021-04-11 01:44:24 +0800

    aaa

---
+++ install_shell/install-init.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+##################################################
+#
+#Auther:liuxiang
+#CreateDate:2021.3.29
+#Description:docker安装配置gitlab
+#UpdateDate:2021.3.29
+#
+###################################################
+#配置脚本执行策略
+set -ex 
+set -o pipefail
+
+#安装RZ命令，用以传输文件
+yum -y install lrzsz
